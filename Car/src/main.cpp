2b095d301c6f6b184a94ff8e26f9e3a341c6beeb
Sourish-D <sourish.dhakal1@gmail.com>
2026-08-28 23:15:22 -0400

    Test: Xbox Controller Connection

--- Car/src/main.cpp
+++ Car/src/main.cpp
@@ -1,108 +1,52 @@
 #include <Arduino.h>
-#include <ESP32Servo.h>
+#include <Bluepad32.h>
 
-Servo myServo;
+ControllerPtr myControllers[BP32_MAX_GAMEPADS];
 
-// Compatibility wrapper: some codebases call ledcAttach(channel, pin)
-// while ESP32 Arduino core provides ledcAttachPin(pin, channel).
-// Provide a simple alias to avoid "ledcAttach not declared" errors.
-static inline void ledcAttach(int channel, int pin) {
-  ledcAttachPin(pin, channel);
-}
-
-const int pwmChannel = 6;
-const int IN1 = 26;
-const int IN2 = 25;
-const int IN3 = 33;
-const int IN4 = 32;
-const int servoPin = 14;
-const int mEN  = 15;
-const int mIN1 = 18;
-const int mIN2 = 19;
-
-int stepNumber = 0;
-
-unsigned long lastStepTime = 0;
-int stepDelay = 1000; // microseconds between steps
-
-int sequence[8][4] = {
-  {1,0,0,1},
-  {1,0,0,0},
-  {1,1,0,0},
-  {0,1,0,0},
-  {0,1,1,0},
-  {0,0,1,0},
-  {0,0,1,1},
-  {0,0,0,1}
-};
-
-void setup() {
-  
-  Serial.begin(9600);
-  
-  pinMode(IN1, OUTPUT);
-  pinMode(IN2, OUTPUT);
-  pinMode(IN3, OUTPUT);
-  pinMode(IN4, OUTPUT);
-
-  pinMode(mEN, OUTPUT);
-  pinMode(mIN1, OUTPUT);
-  pinMode(mIN2, OUTPUT);
-
-
-  digitalWrite(mIN1, HIGH);
-  digitalWrite(mIN2, LOW);
-  ledcSetup(pwmChannel, 1000, 8);      // 1 kHz, 8-bit
-  ledcAttachPin(mEN, pwmChannel);
-
-  myServo.attach(servoPin);
-}
-
-void moveStepper() {
-
-  if (micros() - lastStepTime >= stepDelay) {
-
-    lastStepTime = micros();
-
-    digitalWrite(IN1, sequence[stepNumber][0]);
-    digitalWrite(IN2, sequence[stepNumber][1]);
-    digitalWrite(IN3, sequence[stepNumber][2]);
-    digitalWrite(IN4, sequence[stepNumber][3]);
-
-    stepNumber++;
-
-    if (stepNumber > 7)
-      stepNumber = 0;
+void onConnectedController(ControllerPtr ctl) {
+  for (int i = 0; i < BP32_MAX_GAMEPADS; i++) {
+    if (myControllers[i] == nullptr) {
+      Serial.printf("Connected: %s\n", ctl->getModelName().c_str());
+      myControllers[i] = ctl;
+      break;
+    }
   }
 }
 
-void loop() {
-  moveStepper();
-/*
-  int potValue = analogRead(potPin);
-  
-  int angle = map(potValue, 0, 4095, 90, 1);
-  Serial.print("Pot: ");
-  Serial.print(potValue);
-  Serial.print("  Angle: ");
-  Serial.println(angle);
-  */
-
-  myServo.write(45);
-
+void onDisconnectedController(ControllerPtr ctl) {
+  for (int i = 0; i < BP32_MAX_GAMEPADS; i++) {
+    if (myControllers[i] == ctl) {
+      Serial.println("Controller disconnected.");
+      myControllers[i] = nullptr;
+      break;
+    }
+  }
 }
-
-
-/* Buzzer Code
-#include <Arduino.h>
-const int D25 = 25;
 
 void setup() {
-  pinMode(D25, OUTPUT);
+  Serial.begin(115200);
+  Serial.println("Starting Bluepad32...");
+
+  BP32.setup(&onConnectedController, &onDisconnectedController);
+  BP32.forgetBluetoothKeys();
+  BP32.enableVirtualDevice(false);
 }
 
 void loop() {
-  tone(D25, 1000);
-  delay(25);
+  BP32.update();
+
+  for (auto ctl : myControllers) {
+    if (ctl && ctl->isConnected() && ctl->hasData()) {
+      if (ctl->a()) Serial.println("Button A Pressed!");
+      if (ctl->b()) Serial.println("Button B Pressed!");
+
+      static unsigned long lastPrint = 0;
+      if (millis() - lastPrint > 500) {
+        Serial.printf("LX: %d | LY: %d\n", ctl->axisX(), ctl->axisY());
+        lastPrint = millis();
+      }
+    }
+  }
+
+  delay(15);
 }
-*/ 
